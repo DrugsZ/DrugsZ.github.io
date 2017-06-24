d4a606929eaa39bc8a15f7e68dc64ab6b5896e77
DrugsZ <1002881508@qq.com>
2017-06-25 07:17:55 +0800

    更新ui，及控制流程，优化命令执行过程中的界面效果

---
+++ task36/app/command.js
@@ -0,0 +1,142 @@
+/**
+ * Created by DrugsZ on 2017/6/10.
+ */
+var CreateCar=require('./CreateCar.js');
+CreateCar.CreateNewCar();
+var textarea=document.querySelector('textarea');
+var rowList=document.querySelector('.wrapper');
+var value=textarea.value;
+var timer=null;
+var arr=value.split('\n');
+var execBtn=document.querySelector('#execute');
+var refreshBtn=document.querySelector('#refresh');
+tTimer=setInterval(getData,500);
+execBtn.addEventListener('click',function(){
+
+    execute();
+},false);
+textarea.addEventListener('keydown',function(){
+    getData();
+},false);
+textarea.addEventListener('scroll',function(){
+    rowList.style.top=-textarea.scrollTop+'px';
+},false);
+var commandTest= {
+    GO: /^go(\s+)?(\d+)?$/i,
+    TRA: /^tra\s+(bac|lef|top|rig)(\s+)?(\w+)?$/i,
+    MOV: /^mov\s+(bac|lef|top|rig)(\s+)?(\w+)?$/i
+};
+
+function execute(){
+    clearInterval(timer);
+    var i=0;
+    arr=getData();
+    timer=setInterval(function(){
+        if(i>=arr.length-1){
+            clearInterval(timer);
+        }
+        run(arr[i],i);
+        i++
+    },1000)
+}
+function run(msg,i){
+    var command=getCommand(msg,i);
+    var msg=msg.trim();
+    if(!command){
+        return false;
+    }
+    var isFalse=CreateCar.receive(command);
+    setColor('select',i);
+    if(!isFalse){
+        clearInterval(timer);
+        setColor('error',i);
+        return false;
+    }
+}
+function getCommand(msg,i){
+    var arr=[];
+    var command={};
+    var msg=msg.toUpperCase();
+    if(msg.match(commandTest.GO)){
+        arr=msg.match(commandTest.GO);
+        arr.shift();
+        if(!isNaN(arr[arr.length-1])) {
+            command = CreateCommand('GO', arr[arr.length-1])
+        }else{
+            command=CreateCommand('GO',1)
+        }
+    }else if(msg.match(commandTest.TRA)){
+        arr=msg.match(commandTest.TRA);
+        arr.shift();
+        if(!isNaN(arr[arr.length-1])) {
+            command = CreateCommand('TRA',arr[0], arr[arr.length-1])
+        }else{
+            command=CreateCommand('TRA',arr[0],1)
+        }
+    }else  if(msg.match(commandTest.MOV)){
+        arr=msg.match(commandTest.MOV);
+        arr.shift();
+        if(!isNaN(arr[arr.length-1])) {
+            command = CreateCommand('MOV',arr[0], arr[arr.length-1])
+        }else{
+            command=CreateCommand('MOV',arr[0],1)
+        }
+    }else {
+        command=null;
+    }
+    if(command==null){
+        console.log('指令有误，请确认后重新操作');
+        clearInterval(timer);
+        setColor('error',i);
+        return false;
+    }else{
+        return command;
+    }
+}
+function CreateCommand(){
+    var command={};
+    if(arguments.length==2){
+        command.way=arguments[0];
+        command.num=arguments[1];
+    }else if(arguments.length==3){
+        command.way=arguments[0];
+        command.dir=arguments[1];
+        command.num=arguments[2];
+    }
+    return command;
+}
+function setRowList(num){
+    var rowlist=document.querySelector('.wrapper');
+    var i=rowlist.childNodes.length;
+    var tempArr='';
+    if(i==num){
+        return false;
+    }else{
+        for(var j=0;j<num;j++){
+            tempArr+='<div><span>'+(j+1)+'</span></div>';
+        }
+    }
+    rowlist.innerHTML=tempArr;
+}
+function getData(){
+        value=document.querySelector('textarea').value;
+        arr=value.split('\n');
+        setRowList(arr.length);
+         return arr;
+}
+//type:select,error
+function setColor(type,num){
+    if(num>27&&rowList.childNodes.length-num>27){
+        rowList.style.top=-rowList.childNodes[0].clientHeight*num+'px';
+        textarea.scrollTop=-(rowList.offsetTop);
+    }
+    var arr=rowList.childNodes;
+    for(var i=0;i<arr.length;i++){
+        arr[i].className='';
+    }
+    if(type=='select'){
+        arr[num].className='selectIndex';
+    }else if(type=='error'){
+        arr[num].className='error';
+    }
+}
